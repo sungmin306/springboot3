185c1acb041a92f7280d55499ea88cafdec16fe1
sungmin306 <another0306@naver.com>
2023-09-27 11:16:44 +0900

    9월 27일 스프링 부트 3 백엔드 개발자 되기 시작 맥

---
+++ springbootdeveloper/src/main/java/me/shinsunyoung/springbootdeveloper/SpringbootdeveloperApplication.java
@@ -0,0 +1,13 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdeveloperApplication.class, args);
+	}
+
+}
